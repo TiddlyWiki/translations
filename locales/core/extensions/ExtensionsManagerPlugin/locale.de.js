887198054a94a9750bb26789406a9cc64d819026
Reto Stauss <reto.stauss@mykolab.ch>
2023-09-08 11:12:23 +0200

    feature: add German translation

---
+++ locales/core/extensions/ExtensionsManagerPlugin/locale.de.js
@@ -0,0 +1,12 @@
+/***
+|Name         |ExtensionsExplorerPluginGermanTranslation|
+|Description  |German translation for [[ExtensionsExplorerPlugin|https://github.com/YakovL/TiddlyWiki_ExtensionsExplorerPlugin]]|
+|PluginVersion|0.6.4|
+|Version      |1.0.0|
+|Requires     |ExtensionsExplorerPlugin|
+***/
+//{{{
+    merge(config.macros.extensionsExplorer.lingo, {
+		backstageButtonLabel: "Erweiterungen"
+    })
+    //}}}
